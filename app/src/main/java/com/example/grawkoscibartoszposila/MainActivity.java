bed373200e3e7f66d661967f68791e70e3c0227e
9578845u <posilabartosz@gmail.com>
2024-10-25 12:32:47 +0200

    GraWKosci

--- app/src/main/java/com/example/grawkoscibartoszposila/MainActivity.java
+++ app/src/main/java/com/example/grawkoscibartoszposila/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.grawkoscibartoszposila;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +11,14 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button;
+    private TextView textView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        };
     }
-}
